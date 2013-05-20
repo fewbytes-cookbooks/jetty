737ece5fd7df55c1856ca8d2320efcec6a143947
Yoni Yalovitsky <yoni@fewbytes.com>
2013-05-20 17:16:16 +0300

    fixes to open the jmx externally

--- recipes/jmx.rb
+++ recipes/jmx.rb
@@ -1,6 +1,14 @@
 #this recipe enables jmx access for jetty
 
 node.default["jetty"]["jetty_extra_args"]["jmx"] = "OPTIONS=Server,jmx etc/jetty-jmx.xml"
+node.default["jetty"]["java_extra_options"]["jmx"] = [
+    "-Dcom.sun.management.jmxremote",
+    "-Xshare:off",
+    "-Dcom.sun.management.jmxremote.ssl=false",
+    "-Dcom.sun.management.jmxremote.authenticate=false",
+    "-Dcom.sun.management.jmxremote.local.only=false",
+    "-Djava.rmi.server.hostname=#{node["fqdn"]}",
+].join(" ")
 
 template ::File.join(node["jetty"]["config_dir"], "jetty-jmx.xml") do
   owner node["jetty"]["user"]
